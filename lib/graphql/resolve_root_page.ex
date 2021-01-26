d5e76413892c25188f56a6595512936790a1d55e
Mayel <git@mayel.space>
2021-01-26 13:40:37 +0100

    filtering

--- lib/graphql/resolve_root_page.ex
+++ lib/graphql/resolve_root_page.ex
@@ -11,8 +11,7 @@ defmodule Bonfire.GraphQL.ResolveRootPage do
     :page_opts,
     :info,
     cursor_validators: [&Pointers.ULID.cast/1],
-    paging_opts: %{default_limit: 5, max_limit: 15},
-    data_filters: []
+    paging_opts: %{default_limit: 5, max_limit: 15}
   ]
 
   alias Bonfire.GraphQL
@@ -25,12 +24,17 @@ defmodule Bonfire.GraphQL.ResolveRootPage do
         page_opts: page_opts,
         info: info,
         paging_opts: opts,
-        cursor_validators: validators,
-        data_filters: data_filters
+        cursor_validators: validators
       }) do
-    with {:ok, page_opts} <- GraphQL.full_page_opts(page_opts, validators, opts) do
-      info2 = Map.take(info, [:context]) |> Map.put(:data_filters, data_filters)
-      apply(module, fetcher, [page_opts, info2])
+
+    with {:ok, page_opts_to_fetch} <- GraphQL.full_page_opts(page_opts, validators, opts) do
+
+      info_to_fetch = Map.take(info, [:context])
+        |> Map.put(:data_filters,
+            Map.drop(page_opts, [:limit, :before, :after])
+          ) #|> IO.inspect
+
+      apply(module, fetcher, [page_opts_to_fetch, info_to_fetch])
     end
   end
 end
