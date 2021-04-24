cb04c2c01b0c6523fa0239d013ffbabb428608f2
Mayel <git@mayel.space>
2021-04-24 14:56:24 +0200

    fix

--- lib/graphql/graphql.ex
+++ lib/graphql/graphql.ex
@@ -71,6 +71,7 @@ defmodule Bonfire.GraphQL do
 
   def current_user(%{context: %{current_user: %{id: id} = current_user}}) when is_binary(id), do: current_user
   def current_user(%{context: %{current_account_id: current_account_id, current_username: current_username}}) when is_binary(current_account_id) and is_binary(current_username), do: Bonfire.GraphQL.Auth.user_by(current_username, current_account_id)
+  # def current_user(debug), do: IO.inspect(current_user_debug: debug)
   def current_user(_), do: nil
 
   def current_user_or(info, value), do: lazy_or(current_user(info), value)
