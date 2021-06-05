eea66ea3b37ea3549dcf72e8e15dd3113044e87c
Mayel <git@mayel.space>
2021-06-05 21:02:48 +0200

    context from surface

--- lib/graphql/graphql.ex
+++ lib/graphql/graphql.ex
@@ -70,7 +70,9 @@ defmodule Bonfire.GraphQL do
   def current_account(_), do: nil
 
   def current_user(%{context: %{current_user: %{id: id} = current_user}}) when is_binary(id), do: current_user
+  def current_user(%{context: %{__context__: %{current_user: %{id: id} = current_user}}}) when is_binary(id), do: current_user
   def current_user(%{context: %{current_account_id: current_account_id, current_username: current_username}}) when is_binary(current_account_id) and is_binary(current_username), do: Bonfire.GraphQL.Auth.user_by(current_username, current_account_id)
+  def current_user(%{context: %{__context__: %{current_account_id: current_account_id, current_username: current_username}}}) when is_binary(current_account_id) and is_binary(current_username), do: Bonfire.GraphQL.Auth.user_by(current_username, current_account_id)
   # def current_user(debug), do: IO.inspect(current_user_debug: debug)
   def current_user(_), do: nil
 
