9ef9fa6f332aa91fd908a7d6be31008961a00a2d
Mayel de Borniol <git@mayel.space>
2022-06-23 13:15:37 +1200

    sdl

--- lib/graphql/dev_tools.ex
+++ lib/graphql/dev_tools.ex
@@ -2,11 +2,10 @@ defmodule Bonfire.API.GraphQL.DevTools do
   use Bonfire.UI.Common.Web, :controller
 
   def schema(conn, _params) do
-    sdl = Absinthe.Schema.to_sdl(Bonfire.Common.Config.get!(:graphql_schema_module))
-    # "schema {
-    #   query {...}
-    # }"
+    text(conn, sdl())
+  end
 
-    text(conn, sdl)
+  def sdl do
+    Absinthe.Schema.to_sdl(Bonfire.Common.Config.get!(:graphql_schema_module))
   end
 end
