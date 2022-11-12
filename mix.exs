7c8268a59a411013d492dda7e1bb117870bc13dd
Mayel de Borniol <git@mayel.space>
2022-11-12 19:06:39 +1300

    Upgrade phoenix

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Bonfire.API.GraphQL.MixProject do
       version: "0.2.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      compilers: [:phoenix] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       deps:
         Mess.deps([
           {:phoenix_live_reload, "~> 1.2", only: :dev},
