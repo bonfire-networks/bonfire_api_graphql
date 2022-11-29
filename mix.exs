c4662f5189d0302a6db430269020ff06434009b5
Mayel de Borniol <git@mayel.space>
2022-11-29 14:26:40 +1300

    Function as multi repo umbrella

--- mix.exs
+++ mix.exs
@@ -1,9 +1,20 @@
-Code.eval_file("mess.exs")
+Code.eval_file("mess.exs", (if File.exists?("../../lib/mix/mess.exs"), do: "../../lib/mix/"))
 
 defmodule Bonfire.API.GraphQL.MixProject do
   use Mix.Project
 
   def project do
+    if File.exists?("../../.is_umbrella.exs") do
+      [
+        build_path: "../../_build",
+        config_path: "../../config/config.exs",
+        deps_path: "../../deps",
+        lockfile: "../../mix.lock"
+      ]
+    else
+      []
+    end
+    ++
     [
       app: :bonfire_api_graphql,
       version: "0.2.0",
