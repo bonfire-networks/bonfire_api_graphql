c42ebd35f63debe140bc922ab61afb02a7894c41
Mayel de Borniol <git@mayel.space>
2025-07-06 08:32:49 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/916

---
+++ lib/rest/masto_compatible/masto_compatible_schema.ex
@@ -0,0 +1,12 @@
+defmodule Bonfire.API.MastoCompatible.Schema do
+  # use Oaskit
+
+  # @api_spec Path.join(:code.priv_dir(:bonfire_api_graphql), "specs/akkoma-openapi.json")
+  # |> File.read!()
+  # |> JSON.decode!()
+
+  # @impl true
+  # def spec do
+  #   @api_spec
+  # end
+end
--- lib/web/masto_compatible_router.ex
+++ lib/web/masto_compatible_router.ex
@@ -3,36 +3,49 @@ defmodule Bonfire.API.GraphQL.MastoCompatible.Router do
 
   defmacro include_masto_api do
     quote do
-      scope "/" do
-        pipe_through([:basic_json, :load_authorization, :load_current_auth])
+      # Define a pipeline for API routes with Oaskit/JSV validation
+      pipeline :masto_api do
+        # plug Oaskit.Plugs.SpecProvider, spec: Bonfire.API.MastoCompatible.Schema
+      end
+
+      scope "/api/v1" do
+        pipe_through([:basic_json, :masto_api, :load_authorization, :load_current_auth])
 
         # add here to override wrong priority order of routes
-        get "/api/v1/accounts/verify_credentials",
+        get "/accounts/verify_credentials",
             Bonfire.API.MastoCompatible.AccountController,
             :verify_credentials
 
-        get "/api/v1/accounts/:id", Bonfire.API.MastoCompatible.AccountController, :show
+        get "/accounts/:id", Bonfire.API.MastoCompatible.AccountController, :show
 
-        get "/api/v1/preferences",
+        get "/preferences",
             Bonfire.API.MastoCompatible.AccountController,
             :show_preferences
 
-        get "/api/v1/instance", Bonfire.API.MastoCompatible.InstanceController, :show
-        get "/api/v2/instance", Bonfire.API.MastoCompatible.InstanceController, :show_v2
+        get "/instance", Bonfire.API.MastoCompatible.InstanceController, :show
 
-        post "/api/v1/apps", Bonfire.API.MastoCompatible.AppController, :create
+        post "/apps", Bonfire.API.MastoCompatible.AppController, :create
 
-        get "/api/v1/timelines/home", Bonfire.API.MastoCompatible.TimelineController, :home
-        get "/api/v1/timelines/:feed", Bonfire.API.MastoCompatible.TimelineController, :timeline
-
-        # require Apical
-        # Apical.router_from_file(unquote(@api_spec),
-        #   controller: Bonfire.API.MastoCompatible,
-        #   nest_all_json: false, # If enabled, nest all json request body payloads under the "_json" key. Otherwise objects payloads will be merged into `conn.params`.
-        #   root: "/", 
-        #   dump: :all # temp: ony for debug
-        # )
+        get "/timelines/home", Bonfire.API.MastoCompatible.TimelineController, :home
+        get "/timelines/:feed", Bonfire.API.MastoCompatible.TimelineController, :timeline
       end
+
+      scope "/api/v2" do
+        pipe_through([:basic_json, :masto_api, :load_authorization, :load_current_auth])
+
+        get "/instance", Bonfire.API.MastoCompatible.InstanceController, :show_v2
+      end
+
+      # scope "/" do
+      # pipe_through([:basic_json, :load_authorization, :load_current_auth])
+      # require Apical
+      # Apical.router_from_file(unquote(@api_spec),
+      #   controller: Bonfire.API.MastoCompatible,
+      #   nest_all_json: false, # If enabled, nest all json request body payloads under the "_json" key. Otherwise objects payloads will be merged into `conn.params`.
+      #   root: "/", 
+      #   dump: :all # temp: ony for debug
+      # )
+      # end
     end
   end
 end
